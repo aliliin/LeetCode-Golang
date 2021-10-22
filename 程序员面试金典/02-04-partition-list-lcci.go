7a7a7cfcc28630bfc9b796195affe26a5ee940ff
aliliin <phperali@gmail.com>
2021-10-22 17:38:35 +0800

    02-04-partition-list-lcci.go

---
+++ 程序员面试金典/02-04-partition-list-lcci.go
@@ -0,0 +1,21 @@
+package main
+
+func partition(head *ListNode, x int) *ListNode {
+	small := &ListNode{}
+	smallHead := small
+	large := &ListNode{}
+	largeHead := large
+	for head != nil {
+		if head.Val < x {
+			small.Next = head
+			small = small.Next
+		} else {
+			large.Next = head
+			large = large.Next
+		}
+		head = head.Next
+	}
+	large.Next = nil
+	small.Next = largeHead.Next
+	return smallHead.Next
+}
