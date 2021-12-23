3adb9bed470942c9f67205740712e32180bb1356
aliliin <phperali@gmail.com>
2021-12-23 15:08:18 +0800

    bubbleSort 冒泡排序

--- 常用排序/bubble_sort.go
+++ 常用排序/bubble_sort.go
@@ -16,9 +16,74 @@ func main() {
 		data = append(data, rand.Intn(1000))
 	}
 
-	BubbleSort(data)
+	BubbleSort3(data)
 
 	fmt.Println(data)
+
+	list := []int{5, 9, 1, 6, 8, 14, 6, 49, 25, 4, 6, 3}
+	BubbleSort1(list)
+	fmt.Println(list)
+
+	BubbleSort2(list)
+	fmt.Println(list)
+}
+
+func BubbleSort3(data []int) {
+	if data == nil || len(data) <= 1 {
+		return
+	}
+	l := len(data)
+	swap := false
+	for i := 0; i < l; i++ {
+		for j := 0; j < l-i-1; j++ {
+			if data[j] > data[j+1] {
+				data[j], data[j+1] = data[j+1], data[j]
+				swap = true
+			}
+		}
+	}
+	if !swap {
+		return
+	}
+}
+
+func BubbleSort2(array []int) {
+	n := len(array)
+	swap := false
+
+	for i := n - 1; i > 0; i-- {
+		for j := 0; j < i; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+				swap = false
+			}
+		}
+	}
+
+	if !swap {
+		return
+	}
+
+}
+
+func BubbleSort1(list []int) {
+	n := len(list)
+	// 在一轮中有没有交换过
+	didSwap := false
+	// 进行 N-1 轮迭代
+	for i := n - 1; i > 0; i-- {
+		for j := 0; j < i; j++ {
+			// 如果前面的数比后面的大，那么交换
+			if list[j] > list[j+1] {
+				list[j], list[j+1] = list[j+1], list[j]
+				didSwap = true
+			}
+		}
+	}
+	// 如果在一轮中没有交换过，那么已经排好序了，直接返回
+	if !didSwap {
+		return
+	}
 }
 
 func BubbleSort(data []int) {
@@ -29,7 +94,6 @@ func BubbleSort(data []int) {
 	swap := true
 	for i := 0; i < len(data) && swap; i++ {
 		swap = false
-
 		for j := 0; j < len(data)-i-1; j++ {
 			if data[j] > data[j+1] {
 				data[j], data[j+1] = data[j+1], data[j]
